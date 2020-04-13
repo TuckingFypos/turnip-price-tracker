7b8ca7c0ded325d42f17aa4cb39f5e712144fc12
James DeLayo <james.delayo@gmail.com>
2020-04-13 18:44:24 -0400

    Implements AM/PM timechecks to solve the noon reset issue

--- LillyMain.py
+++ LillyMain.py
@@ -1,12 +1,12 @@
 import datetime
-import discord
 from discord.ext import commands
 import random
 import RoseSecrets
 import copy
+import time
 
 bot = commands.Bot(command_prefix='!')
-turnips_list = [0,'',0,0]
+turnips_list = [0,'',0,0,0]
 turnips_initial = copy.deepcopy(turnips_list)
 turnip_id = 0000
 
@@ -37,22 +37,30 @@ async def turnips(ctx, arg = 0):
     today = datetime.date.today()
     today_day = today.day
     today_month = today.month
+    today_hour = time.localtime().tm_hour
     name = str(ctx.message.author)
 
     if turnips_list[3] < today_day:
         turnips_list[0] = 0
+    # AM PM reset, ensures any submission after noon is accepted when compared to a submission made before noon.
+    if (turnips_list[4] < 12) & (today_hour >= 12):
+        turnips_list[0] = 0
+        turnips_list[3] = 0
 
     if arg == "clear":
         # Sets turnips_list to its initial values
         turnips_list = copy.deepcopy(turnips_initial)
         await ctx.send("Turnip price list cleared.")
+        # User Turnip price submission
     elif arg != 0:
         price = int(arg)
+
         if price >= turnips_list[0]:
             turnips_list[0] = price
             turnips_list[1] = name
             turnips_list[2] = today_month
             turnips_list[3] = today_day
+            turnips_list[4] = today_hour
             await ctx.send(name + " submitted their turnips at " + str(int(arg)) + " bells.")
         else:
             await ctx.send("Your turnips at " + str(price) + " bells are less than the current top-seller.")
@@ -65,7 +73,6 @@ async def turnips(ctx, arg = 0):
         else:
             await ctx.send("No turnip prices registered for today. ")
 
-    #TODO: Reset turnip_list to original state at 5AM; announce the reset in the Switch channel
     #TODO: Send a 15-min warning message to the Switch channel at 9:45PM EST that the shop is about to close
     #TODO: Create a help command that explains the Turnip economy and why the bot exists
 
