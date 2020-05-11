1e22a767983a83cb2bb2ffda0137a9a7cd94b838
James DeLayo <TuckingFypos@users.noreply.github.com>
2020-05-11 16:34:19 -0400

    Adds Dice roll and Quick poll commands
    Adds the commands for rolling dice and starting a poll. 
    Hopefully.

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,6 @@
+import discord
 import random
+import re
 from discord.ext import commands
 
 class Gaming(commands.Cog):
@@ -17,6 +19,56 @@ class Gaming(commands.Cog):
     async def choose(self, ctx, *args):
         decision = random.choice(args)
         await ctx.send("I have chosen " + decision)
+
+    # Rolls any ndn sided dice.
+    @commands.command()
+    async def roll(self, ctx, arg = "1d6"):
+        arg.lower()
+        # Use a regular expression to split the string, then turn both fields into ints
+        player_roll = re.split(r"d", arg)
+        player_roll[0] = int(player_roll[0])
+        player_roll[1] = int(player_roll[1])
+        # Roll the dice and count down the total rolls
+        while player_roll[0] > 0:
+            roll = random.randrange(1, player_roll[1])
+            await ctx.send("Rolling dice {} ... rolled a {}.".format(player_roll[0], roll))
+            player_roll[0] -= 1
+
+    #Creates a Quickpoll
+    @commands.command()
+    async def quickpoll(self, ctx, arg: str):
+        #Use RE to cut the question out of the arg String
+        question = re.match(r'^.+[?]', arg)
+        #Use RE to cut the options out of the arg String
+        raw_solutions = re.match(r'[?]\s(.+)$', arg)
+        #Format the options into a List by splitting the String
+        options = str(raw_solutions.groups(1)).split(',')
+
+        if len(options) <= 1:
+            await ctx.send('You need more than one option to make a poll!')
+            return
+        if len(options) > 10:
+            await ctx.send('You cannot make a poll for more than 10 things!')
+            return
+
+        if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
+            reactions = ['✅', '❌']
+        else:
+            reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
+
+        description = []
+        for x, option in enumerate(options):
+            description += '\n {} {}'.format(reactions[x], option)
+        embed = discord.Embed(
+            title=question,
+            description=''.join(description)
+        )
+        react_message = await ctx.send(embed=embed)
+        for reaction in reactions[:len(options)]:
+            await self.bot.add_reaction(react_message, reaction)
+        embed.set_footer(text='Poll ID: {}'.format(react_message.id))
+        await self.bot.edit_message(react_message, embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Gaming(bot))
