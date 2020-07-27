52c2acb7a764ba912ce710f9211485ddff93d81e
James DeLayo <james.delayo@gmail.com>
2020-07-27 18:16:57 -0400

    Reformats Xur output to an embedded message.

--- cogs/destiny.py
+++ cogs/destiny.py
@@ -12,11 +12,27 @@ class Destiny(commands.Cog):
     async def Xur(self, ctx):
         # Gets Xur info from the wtfix.gg API
         r = requests.get('https://wtfix.gg/api/xur')
+        # Loads the returned JSON to a python dict
         xur_info = json.loads(r.text)
+        # Checks if Xur is found
         if xur_info['found']:
-            await ctx.send("Xur is creepin in {} on {}".format(xur_info['zone'], xur_info['planet']))
+            # If found, prints Xur's location to a Discord Embed
+            embed = discord.Embed(
+                title='Xur Located',
+                colour=discord.Colour.green()
+            )
+            embed.add_field(name='Planet:', value=str(xur_info['planet']), inline=True)
+            embed.add_field(name='Zone:', value=str(xur_info['zone']), inline=True)
+            await ctx.send(embed=embed)
         else:
-            await ctx.send("Doesn't look like Xur is around today (at least, we haven't found him!)")
+            # No Xur? No problem
+            embed = discord.Embed(
+                title='Xur not found',
+                colour=discord.Color.dark_green()
+            )
+            embed.add_field(name='Planet:', value="unknown", inline=True)
+            embed.add_field(name='Zone:', value="unknown", inline=True)
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
